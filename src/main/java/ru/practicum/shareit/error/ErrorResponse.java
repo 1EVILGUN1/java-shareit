226fd1a112661e166b495038e620386b890494f4
YG <yasha.golotin@mail.ru>
2024-10-06 08:35:41 +0300

    update errorresponse
    аннотация getter

--- src/main/java/ru/practicum/shareit/error/ErrorResponse.java
+++ src/main/java/ru/practicum/shareit/error/ErrorResponse.java
@@ -1,7 +1,4 @@
 package ru.practicum.shareit.error;
-
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
 public record ErrorResponse(String error, HttpStatus status, String description) {}
